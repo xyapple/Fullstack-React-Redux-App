e7839623a3db3b554248d6a3a9da160f0a365711
xyapple <ydeascentis@gmail.com>
2017-07-19 08:34:40 -0400

    Changing the timerdashboard props

--- src/components/timerApp/TimersDashboard.js
+++ src/components/timerApp/TimersDashboard.js
@@ -2,13 +2,42 @@
 import React, {Component} from 'react';
 import TimerList from './TimerList';
 import ToggleableTimerForm from './ToggleableTimerForm';
+// import uuid from './vendor/uuid.js';
+const uuidv4 = require('uuid/v4');
 
 class  TimersDashboard extends Component{
+    state={
+        timers:[
+        {
+            title:'clean garden',
+            project:'Summer garden',
+            id: uuidv4,
+            elapsed: 5456099,
+            runningSince: Date.now()
+            },
+         {
+            title: 'Bake squash',
+            project: 'Kitchen Chores',
+            id: uuidv4,
+            elapsed: 1273998,
+            runningSince: null,
+          },
+          {
+            title: 'Practice squat',
+            project: 'Gym Chores',
+            id: uuidv4,
+            elapsed: 5456099,
+            runningSince: Date.now(),
+          }
+        ]
+    }
+
     render(){
         return(
             <div className='ui three column centered grid'>
                 <div className='column'>
-                    <TimerList />
+                    <TimerList
+                        timers={this.state.timers}/>
                     <ToggleableTimerForm isOpen={true} />
                 </div>
             </div>
