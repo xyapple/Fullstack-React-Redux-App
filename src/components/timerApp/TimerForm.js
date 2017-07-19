8f12c774686325c8f303980d1f439cf1cd963146
xyapple <ydeascentis@gmail.com>
2017-07-19 09:11:41 -0400

    change time form to statful component

--- src/components/timerApp/TimerForm.js
+++ src/components/timerApp/TimerForm.js
@@ -2,6 +2,17 @@
 import React, {Component} from 'react';
 
 class TimerForm extends Component {
+    state={
+        title:this.props.title ||'',
+        project:this.props.project ||'',
+    }
+    handleTitleChange=(e)=>{
+        this.setState({title:e.target.value});
+
+    }
+    handleProjectChange=(e)=>{
+        this.setState({project:e.target.value});
+    }
     render(){
         const submitText = this.props.title ? 'Update' : 'Create';
         return(
@@ -10,14 +21,21 @@ class TimerForm extends Component {
                     <div className='ui form'>
                         <div className='field'>
                             <label >Title</label>
-                            <input type='text' defaultValue={this.props.title}/>
+                            <input type='text'
+                            value={this.state.title}
+                            onClick={this.handleTitleChange}/>
                         </div>
                         <div className='field'>
                             <label >Project</label>
-                            <input type='text' defaultValue={this.props.project}/>
+                            <input type='text'
+                                value={this.state.project}
+                                onClick={this.handleProjectChange}
+                            />
                         </div>
                         <div className='ui two bottom attached buttons'>
-                            <button className='ui basic blue button'>
+                            <button
+                                className='ui basic blue button'
+                                >
                                 {submitText}
                             </button>
                             <button className='ui basic red button'>
